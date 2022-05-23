84ce6e91dedd800820332e500db84d6660a1a908
Costa Tsaousis <costa@netdata.cloud>
2022-05-23 12:59:32 +0300

    faster averaging

--- web/api/queries/average/average.c
+++ web/api/queries/average/average.c
@@ -5,39 +5,46 @@
 // ----------------------------------------------------------------------------
 // average
 
-struct grouping_average {
+static __thread struct grouping_average {
+    long resampling_group;
+    calculated_number resampling_divisor;
     calculated_number sum;
     size_t count;
-};
+} *g = NULL;
 
 void *grouping_create_average(RRDR *r) {
     (void)r;
-    return callocz(1, sizeof(struct grouping_average));
+    g = callocz(1, sizeof(struct grouping_average));
+    g->resampling_divisor = r->internal.resampling_divisor;
+    g->resampling_group = r->internal.resampling_group;
+    return g;
 }
 
 // resets when switches dimensions
 // so, clear everything to restart
 void grouping_reset_average(RRDR *r) {
-    struct grouping_average *g = (struct grouping_average *)r->internal.grouping_data;
+    // struct grouping_average *g = (struct grouping_average *)r->internal.grouping_data;
     g->sum = 0;
     g->count = 0;
 }
 
 void grouping_free_average(RRDR *r) {
-    freez(r->internal.grouping_data);
+    freez(g);
+    // freez(r->internal.grouping_data);
     r->internal.grouping_data = NULL;
+    g = NULL;
 }
 
 void grouping_add_average(RRDR *r, calculated_number value) {
     if(likely(!isnan(value))) {
-        struct grouping_average *g = (struct grouping_average *)r->internal.grouping_data;
+        // struct grouping_average *g = (struct grouping_average *)r->internal.grouping_data;
         g->sum += value;
         g->count++;
     }
 }
 
 calculated_number grouping_flush_average(RRDR *r,  RRDR_VALUE_FLAGS *rrdr_value_options_ptr) {
-    struct grouping_average *g = (struct grouping_average *)r->internal.grouping_data;
+    // struct grouping_average *g = (struct grouping_average *)r->internal.grouping_data;
 
     calculated_number value;
 
@@ -46,11 +53,11 @@ calculated_number grouping_flush_average(RRDR *r,  RRDR_VALUE_FLAGS *rrdr_value_
         *rrdr_value_options_ptr |= RRDR_VALUE_EMPTY;
     }
     else {
-        if(unlikely(r->internal.resampling_group != 1)) {
+        if(unlikely(g->resampling_group != 1)) {
             if (unlikely(r->result_options & RRDR_RESULT_OPTION_VARIABLE_STEP))
-                value = g->sum / g->count / r->internal.resampling_divisor;
+                value = g->sum / g->count / g->resampling_divisor;
             else
-                value = g->sum / r->internal.resampling_divisor;
+                value = g->sum / g->resampling_divisor;
         } else
             value = g->sum / g->count;
     }
