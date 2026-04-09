9d6e178898cbbd05aa1105c9ee4d60ad832c1baf
Costa Tsaousis <costa@netdata.cloud>
2026-04-10 01:09:22 +0300

    go.d/snmp: fix virtual metric validation license header

--- src/go/plugin/go.d/collector/snmp/ddsnmp/ddprofiledefinition/virtual_metric_dim_validation.go
+++ src/go/plugin/go.d/collector/snmp/ddsnmp/ddprofiledefinition/virtual_metric_dim_validation.go
@@ -1,7 +1,4 @@
-// Unless explicitly stated otherwise all files in this repository are licensed
-// under the Apache License Version 2.0.
-// This product includes software developed at Datadog (https://www.datadoghq.com/).
-// Copyright 2016-present Datadog, Inc.
+// SPDX-License-Identifier: GPL-3.0-or-later
 
 package ddprofiledefinition
 
